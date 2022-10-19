d548b1edf803376a5db73cae94b6bef869eb5dae
Ilay Sokolov <sokol8807@gmail.com>
2022-10-19 06:56:52 +0300

    Initial Commit

---
+++ TraficLightSwiftUI/TraficLightSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TraficLightSwiftUIApp.swift
+//  TraficLightSwiftUI
+//
+//  Created by Ilya Sokolov on 19.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct TraficLightSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
